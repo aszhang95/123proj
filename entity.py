85e7645c0306ca51b44d22426f0388ff54af6e77
Steven Wei <srwei@cs.uchicago.edu>
2017-06-01 12:01:08 -0500

    ok

--- entity.py
+++ entity.py
@@ -1,7 +1,7 @@
 import json
 import requests as req
 import spacy
-import spacy
+import re
 from textblob.en.sentiments import PatternAnalyzer
 import numpy as np
 
@@ -14,31 +14,30 @@ types = {'Person':'PERSON', 'Event':'EVENT', 'Organization':'ORG', 'Place':'LOC'
 def sentiment(comment, subjectivity = True):
     doc = nlp(comment)
 
-    outs = np.zeros(len(doc.ents), dtype = tuple)
+    outs = []
 
     for ind, entity in enumerate(doc.ents):
         if entity.label_ in types.values():
             iden, political_entity = is_political(entity.text)
-            analyzed = PatternAnalyzer.analyze(PatternAnalyzer, str(entity.sent))
-            score = None
+            if iden:
+                analyzed = PatternAnalyzer.analyze(PatternAnalyzer, str(entity.sent))
+                score = None
 
-            if subjectivity:
-                score = analyzed.polarity * analyzed.subjectivity
-            else:
-                score = analyzed.polarity
+                if subjectivity:
+                    score = analyzed.polarity * analyzed.subjectivity
+                else:
+                    score = analyzed.polarity
 
-            outs.append((political_entity, score))
+                outs.append((political_entity, score))
 
     return outs
-
-
-
 
 
 def is_political(ent_text):
 
     api_key = 'AIzaSyAMSkyNxAUbhtlvfWOKGJAO8w1hbj2WXC0'
     service_url = 'https://kgsearch.googleapis.com/v1/entities:search'
+    ent_text = re.sub("[^\w]", "", ent_text)
     params = [
         ('query', ent_text),
         ('limit', 5),
@@ -51,26 +50,53 @@ def is_political(ent_text):
     data = json.loads(page.text)
     out = None
     score = 0
-    for item in data['itemListElement']:
-        description = None
-        try:
-            description = item['result']['detailedDescription']['articleBody'] + ' ' + item['result']['name']
+    political_entity = False
+    if 'itemListElement' in data.keys():
+        for item in data['itemListElement']:
+            description = None
+            try:
+                description = item['result']['detailedDescription']['articleBody']
 
-        except:
-            description = item['result']['description']
+            except:
+                try:
+                    description = item['result']['description']
+                except:
+                    print('\nERROR: NO DESCRIPTION')
+                    print(item)
+                    description = ''
+            try:
+                description += ' ' + item['result']['name']
+            except:
+                pass
 
-        political = sum([word in description for word in words])
+            political = sum([word in description for word in words])
 
-        right_type = sum([t in types.keys() for t in item['result']['@type']])
+            try:
+                right_type = sum([t in types.keys() for t in item['result']['@type']])
+            except:
+                print('\nERROR IN TYPE')
+                print(ent_text)
+                print(item)
 
-        if political and right_type:
-            if political > score:
-                out = (item['result']['@id'], item['result']['name'])
-                score = political
-                political_entity = True
-    
+            if political and right_type:
+                if political > score:
+                    try:
+                        out = (item['result']['@id'], item['result']['name'])
+                        score = political
+                        political_entity = True
+                    except:
+                        print('\nERROR IN ID/NAME')
+                        print(item)
+    else:
+        print('\nERROR: NO RESULTS FOR {}'.format(ent_text))
+        print(data)
+            
     if not out:
-        entity = data['itemListElement'][0]['result']
-        out = (entity['@id'], entity['name'])
-        political_entity = False
-    return out, political_entity
+        try:
+            entity = data['itemListElement'][0]['result']
+            out = (entity['@id'], entity['name'])
+        except:
+            print('\n NOTHING FOUND FOR ENTITY: {}'.format(ent_text))
+            print(data)
+
+    return (out, political_entity)
